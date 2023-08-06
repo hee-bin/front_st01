949b753f378255efac7d7819f603179862188aef
hee-bin <gmlqls4732@naver.com>
2023-08-07 08:12:11 +0900

    js01

---
+++ js01/day01.js
@@ -0,0 +1,64 @@
+console.log('hello');
+
+
+/*
+1. 변수 선언
+ let, var
+
+ 상수 : const
+
+*/ 
+
+const irum='Jung Hee Bin';
+// irum='Park!'; const는 재할당을 할 수 없다.
+// console.log('new name', irum);
+
+/* var a=10;
+console.log(a);
+
+a=20;
+console.log('new-a',a);
+
+console.log('---------')
+console.log
+
+console.log('---------')
+
+su=10;
+console.log(su); */
+
+
+/* 
+프로그래밍할 땐 let을 쓰는 것이 좋다. 미리 선언을 해야만 함.
+절대 바꾸지 않을 값을 쓸 땐 안정적으로 const를 쓰자. */
+/* 
+let r;
+r=[1,2,3,4,5,];
+console.log(r);
+
+
+let a1=10;
+let b='222'
+let c=null;
+let d= 20==100;
+let e;
+console.log(typeof a1.'a1');
+console.log(typeof b,'b');
+console.log(typeof c,'c');
+console.log(typeof d,'d');
+console.log(typeof e,'e');
+
+console.log(typeof 10/0, '10/0');
+console.log(typeof 10/0, '10/0');
+console.log(typeof 10/0, '10/0');
+ */
+
+let t1=[]; //배열
+let t2={}; //객체
+let t3=function(){
+    console.log(test);
+}; // 함수
+
+console.log(typeof t1);
+console.log(typeof t2);
+console.log(typeof t3);
